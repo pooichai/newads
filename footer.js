0ea1ef3c091a5540bcd09466ac2e76fe5505b8e2
pooichai <pooichai7@gmail.com>
2024-04-17 06:19:14 +0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,7 +1,6 @@
 document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999">');
 document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
 
-//Paste Iklan Disini
 
 <script type="text/javascript">
 	atOptions = {
@@ -13,3 +12,7 @@ document.write('<div style="text-align:center;display:block;max-width:729px;heig
 	};
 </script>
 <script type="text/javascript" src="//www.topcreativeformat.com/ea7073a5f88001ce3ea7a9725829313d/invoke.js"></script>
+
+
+document.write('</div>');
+document.write('</div>');
